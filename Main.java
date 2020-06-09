a12dc78e3535652b9e621962ebaec532358741d4
Sara Mohammadi <9810sara.m@gmail.com>
2020-06-09 12:51:51 +0430

    main.java commited

---
+++ Main.java
@@ -0,0 +1,14 @@
+
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+	// changed
+        Shape s1 = new Rectangle("Yellow", 2, 4);
+        Shape s2 = new Rectangle("Yellow", 2, 4);
+		//add
+        System.out.println("hello");
+        System.out.println(s2.toString());
+    }
+}
